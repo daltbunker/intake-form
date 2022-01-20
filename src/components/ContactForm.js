923f0a11b73a4573b4935093c067d4a7acccdd45
Dalton Bunker <dbbunker3@gmail.com>
2022-01-20 14:15:55 -0700

    add submit/clear event handlers

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,23 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react'
 
 function ContactForm() {
+
+    const defaultFormInput = {
+        name: "",
+        email: "",
+        birthdate: "",
+        contactAgreement: false
+    }
+
+    const [formInput, setFormInput] = useState(defaultFormInput)
+
+    function handleInputChange(e) {
+        const {name, value, checked} = e.target
+        setFormInput(prevFormInput => {
+            return {
+                ...prevFormInput,
+                [name]: name === "contactAgreement" ? checked : value
+            }
+        })
+    }
 
     function handleFormSubmit(e) {
         e.preventDefault()
     }
+
     return(
         <div className="ContactForm">
             <h2>Contact Us</h2>
             <form onSubmit={(e) => handleFormSubmit(e)}>
-                <label htmlFor="">Name</label>
-                <input type="text" required />
-                <label htmlFor="">Email</label>
-                <input type="email" pattern=".+@.+\.com" required />
-                <label htmlFor="">Birthdate</label>
-                <input type="date" />
-                <label htmlFor="">I agree to be contaced via email</label>
-                <input type="checkbox" required />
-                <button>SUBMIT</button>
+                <div className="input-container">
+                    <label>Name</label>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        value={formInput.name} 
+                        onChange={(e) => handleInputChange(e)} 
+                        required 
+                    />
+                </div>
+                <div className="input-container">
+                    <label>Email</label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        value={formInput.email} 
+                        onChange={(e) => handleInputChange(e)} 
+                        pattern=".+@.+\.[A-Za-z]{2,4}" 
+                        required 
+                    />
+                </div>
+                <div className="input-container">
+                    <label>Birthdate</label>
+                    <input 
+                        type="date" 
+                        name="birthdate" 
+                        value={formInput.birthdate} 
+                        onChange={(e) => handleInputChange(e)} 
+                    />
+                </div>
+                <div className="input-container">
+                    <label>I agree to be contacted via email</label>
+                    <input 
+                        type="checkbox" 
+                        name="contactAgreement" 
+                        value={formInput.contactAgreement} 
+                        onChange={(e) => handleInputChange(e)} 
+                        required 
+                    />
+                </div>
+                <button className="form-btn">SUBMIT</button>
+                <button className="form-btn">CLEAR</button>
             </form>
         </div>
     )
